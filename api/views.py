8144ae51658fc517bcf35c29227a92b0b496b76b
Nicola Erario <nicola.erario@gmail.com>
2021-02-10 19:42:23 +0100

    Adds Job Offer list and detail views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from jobs.models import JobOffer
+from .serializers import JobOfferSerializer
 
-# Create your views here.
+
+class JobOfferList(generics.ListCreateAPIView):
+    queryset = JobOffer.objects.all()
+    serializer_class = JobOfferSerializer
+
+
+class JobOfferDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = JobOffer.objects.all()
+    serializer_class = JobOfferSerializer
